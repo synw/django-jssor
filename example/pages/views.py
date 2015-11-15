ec26b8fa3a3d758b4248a8a7745db79356bc9dbd
syn <synwe@yahoo.fr>
2015-11-15 15:31:53 +0000

    Example update

--- example/pages/views.py
+++ example/pages/views.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 
-from django.views.generic.detail import DetailView
+from django.views.generic import TemplateView
+from django.shortcuts import get_object_or_404
 from pages.models import Page
 
 
-class PageView(DetailView):
+class PageView(TemplateView):
     template_name = 'pages/default.html'
-    model = Page
+
+    def get_context_data(self, **kwargs):
+        context = super(PageView, self).get_context_data(**kwargs)
+        page=get_object_or_404(Page, slug=self.kwargs['slug']).select_related('slideshow')
+        context['page'] = page
+        return context
