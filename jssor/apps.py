54fcb879a2cdc689321fdf6be34b890f9bea15ca
syn <synwe@yahoo.fr>
2016-02-19 15:57:05 +0000

    Correction

--- jssor/apps.py
+++ jssor/apps.py
@@ -1,6 +1,6 @@
 from django.utils.translation import ugettext_lazy as _
 from django.apps import AppConfig
 
-class PollsConfig(AppConfig):
+class JssorConfig(AppConfig):
     name = "jssor"
     verbose_name = _("Slideshows")
