9882c6a095bdaeb9e72b45ecbf96c59f0122844b
syn <synwe@yahoo.fr>
2016-06-13 11:15:41 +0000

    Django 1.9 url format

--- jssor/urls.py
+++ jssor/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from jssor.views import SlideshowDispatcherView
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^slideshow/(?P<group_id>[-_\w]+)/(?P<screen_width>[0-9]+)/(?P<screen_height>[0-9]+)/$', SlideshowDispatcherView.as_view(), name="slideshow-dispatcher"),
-)
+    ]
 
